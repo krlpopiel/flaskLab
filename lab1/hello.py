42855ec097dd5fb956ed31214a9110bcb06676ed
krlpopiel <admir0305@gmail.com>
2026-03-18 11:01:51 +0100

    zakonczono zad3

--- lab1/hello.py
+++ lab1/hello.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"   
+
+@app.route("/o-mnie")
+def o_mnie():
+    return "<p>Oto krótkie informacje o mnie.</p>"  
+
+@app.route("/user/<name>")
+def user(name):
+    return f"<p>Oto informacje o użytkowniku: {name}</p>"  
